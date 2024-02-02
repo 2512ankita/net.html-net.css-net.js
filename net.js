08fe7c0db55aa2212d4f7d061f912e43940b48c7
2512ankita <121779735+2512ankita@users.noreply.github.com>
2024-02-02 21:48:12 +0530

    Add files via upload

---
+++ net.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded'), function () {
+    const moviesContainer = document.getElementById('moviesContainer');
+  
+   
+    const moviesData = [
+      { title: 'Movie 1', image: 'movie1.jpg' },
+      { title: 'Movie 2', image: 'movie2.jpg' },
+      { title: 'Movie 3', image: 'movie3.jpg' },
+      
+    ];
+}
+  
+  
+  
